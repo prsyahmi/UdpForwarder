b2b75ac867661cad775825b703f02cf3746ec69e
Syahmi Azhar <prsyahmi@gmail.com>
2025-01-17 23:09:39 +0800

    lock due to concurrent

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio::net::UdpSocket;
 use tokio::io::Result;
-use tokio::task;
 
 struct ClientMap {
     client_addr: SocketAddr,
@@ -15,55 +14,83 @@ struct ClientMap {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let socket1 = Arc::new(UdpSocket::bind("127.0.0.1:27088").await?);
+    let socket1 = UdpSocket::bind("127.0.0.1:27088").await?;
+    let sock_arc = Arc::new(socket1);
+    let clients: HashMap<SocketAddr, ClientMap> = HashMap::new();
+    let clients_arc = Arc::new(Mutex::new(clients));
 
-    let clients_back: HashMap<SocketAddr, ClientMap> = HashMap::new();
-    let clients: Arc<Mutex<HashMap<SocketAddr, ClientMap>>> = Arc::new(Mutex::new(clients_back));
-    let clients_clone = Arc::clone(&clients);
+    let clients_clone = Arc::clone(&clients_arc);
 
-    loop {
-        let s = handle_from_client(&socket1, &clients_clone).await;
-        if let Err(e) = s {
-            println!("Error! {}", e);
+    let j = tokio::spawn(async move {
+        loop {
+            let _ = handle_from_client(&sock_arc, &clients_clone).await;
         }
+    });
 
-        let socket1_clone = Arc::clone(&socket1);
-        let clients_clone = Arc::clone(&clients_clone);
+    let _ = tokio::join!(j);
 
-        task::spawn(async move {
-            let mut cs = clients_clone.lock().await;
-            for (key, map) in cs.iter_mut() {
-                let (len, _) = map.server_sock.recv_from(&mut map.buffer).await.unwrap();
-
-                let _ = socket1_clone.send_to(&map.buffer[..len], map.client_addr).await;
-            }
-        });
-    }
+    Ok(())
 }
 
-async fn create_client(addr: SocketAddr) -> ClientMap {
-    ClientMap {
+async fn create_client(addr: SocketAddr, socket: &Arc<UdpSocket>, clients: &Arc<Mutex<HashMap<SocketAddr, ClientMap>>>) -> ClientMap {
+    println!("Create client {}", addr);
+
+    let cm = ClientMap {
         buffer: vec![0u8; 2048],
         client_addr: addr,
         last_activity: 0,
         server_sock: UdpSocket::bind("0.0.0.0:0").await.unwrap(),
-    }
+    };
+
+    let socket_clone = Arc::clone(socket);
+    let clients_clone = Arc::clone(clients);
+
+    tokio::spawn(async move {
+        let _: Result<()> = async move {
+            let mut i = 0;
+            loop {
+                i += 1;
+                println!("Handle from server! {}", i);
+
+                let cs: tokio::sync::MutexGuard<'_, HashMap<SocketAddr, ClientMap>> = clients_clone.lock().await;
+                let client = cs.get(&addr).unwrap();
+                let mut buf = vec![0u8; 2048];
+                let (len, addr) = client.server_sock.recv_from(&mut buf).await?;
+                println!("Received {} bytes from server: {}", len, addr);
+                let r = socket_clone.send_to(&buf[..len], client.client_addr).await?;
+                println!("Sent {} bytes to client", r);
+            }
+
+            Ok(())
+        }.await;
+    });
+
+    return cm;
 }
 
 async fn handle_from_client(socket: &Arc<UdpSocket>, clients: &Arc<Mutex<HashMap<SocketAddr, ClientMap>>>) -> Result<()> {
-    //println!("Handle from client!");
+    println!("Handle from client!");
 
     let mut buf = vec![0u8; 2048];
     let (len, addr) = socket.recv_from(&mut buf).await?;
-    //println!("Received {} bytes from client: {}", len, addr);
+    println!("Received {} bytes from client: {}", len, addr);
 
-    let mut cs = clients.lock().await;
-    let client = cs.entry(addr).or_insert_with(|| {
-        tokio::task::block_in_place(|| futures::executor::block_on(create_client(addr)))
+    let socket_clone = Arc::clone(socket);
+    let clients_clone = Arc::clone(clients);
+
+    tokio::spawn(async move {
+        let mut cs = clients_clone.lock().await;
+        println!("locked");
+        let client = cs.entry(addr).or_insert_with(|| {
+            tokio::task::block_in_place(|| futures::executor::block_on(create_client(addr, &socket_clone, &clients_clone)))
+        });
+
+        let server_addr = SocketAddr::from(([10, 11, 12, 1], 27015));
+        println!("Sending {} bytes to server", len);
+        let r = client.server_sock.send_to(&buf[..len], server_addr).await;
+        //println!("Sent {} bytes to server", r);
+
     });
-
-    let server_addr = SocketAddr::from(([10, 11, 12, 1], 27015));
-    client.server_sock.send_to(&buf[..len], server_addr).await?;
 
     Ok(())
 }
